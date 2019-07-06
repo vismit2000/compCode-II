66b4b4c5f9519cdbe4a3068695993b192016b797
vismit2000 <44922474+vismit2000@users.noreply.github.com>
2019-07-06 21:34:29 +0530

    BST GfG Sudo Placement 2019
    You need to build a BST from the given array A of size N. The array contains distinct elements. You need to build the tree using the Array elements in the order of their arrival (A[0] becomes root and so on). Now, you need to sum the alternate leaf nodes of this BST and print the result.
    Note: Start from the left side of the tree when searching for leaf nodes. So sum leaftnode1+leaftnode3+leafnode5 ...
    
    Input:
    The first line of input contains T denoting the number of testcases. T testcases follow. Each testcase contains two lines of input. The first line contains N. The second line contains array elements.
    
    Output:
    For each testcase, in a new line, print the sum of alternate leaf nodes.
    
    Constraints:
    1 <= T <= 200
    1 <= N <= 107
    1 <= Ai <= 108
    
    Examples:
    Input:
    1
    6
    5 1 2 3 4 6
    Output:
    4
    
    Explanation:
    Testcase1:
    The BST will look like :
                5
              /     \
            1        6
             \         
                2                
                   \          
                    3  
                        \
                            4
    As it is evident from the above BST, the leaf nodes are 4 and 6. So sum of alternate leaf nodes would be 4.

---
+++ Sum_alt_leaf_nodes_BST.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long sum, cnt;
+
+class Node
+{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d)
+        {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution
+{
+public:
+    Node *insert(Node *root, int data)
+    {
+        if(root == NULL)
+            return new Node(data);
+        
+        Node *cur;
+            
+        if(data <= root->data)
+        {
+            cur = insert(root->left, data);
+            root->left = cur;
+        }
+        else
+        {
+            cur = insert(root->right, data);
+            root->right = cur;
+        }
+        return root;
+    }
+    
+    void inorder(Node *root)
+    {
+        if(root == NULL)
+            return;
+        if(root->left == NULL && root->right == NULL)
+        {
+            if(cnt % 2 == 0)
+                sum += root->data;
+            cnt++;
+        }
+        inorder(root->left);
+        inorder(root->right);
+    }
+};
+
+int main() 
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    int data;
+	    Solution myTree;
+	    Node *root = NULL;
+	    
+	    for(int i = 0; i < n; i++)
+	    {
+	        cin >> data;
+	        root = myTree.insert(root, data);
+	    }
+	    
+	    sum = 0;
+	    cnt = 0;
+	    
+	    myTree.inorder(root);
+	    
+	    cout << sum << endl;
+	    
+	}
+	return 0;
+}
