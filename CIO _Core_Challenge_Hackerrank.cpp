5cc14890bd053ad7a00dd6e2f0adac7f4700b314
vismit2000 <44922474+vismit2000@users.noreply.github.com>
2019-07-24 08:33:11 +0530

    Update CIO _Core_Challenge_Hackerrank.cpp

--- CIO _Core_Challenge_Hackerrank.cpp
+++ CIO _Core_Challenge_Hackerrank.cpp
@@ -1,6 +1,6 @@
-//Given a directed graph containing cycle. Find minimum distance of every node from cycle.
+//Given an undirected graph containing cycle. Find minimum distance of every node from cycle.
 
-//Concepts used : Finding cycle in a directed graph, multi-source BFS using priority queue
+//Concepts used : Finding cycle in an undirected graph, multi-source BFS using priority queue
 
 #include <bits/stdc++.h>
 using namespace std;
