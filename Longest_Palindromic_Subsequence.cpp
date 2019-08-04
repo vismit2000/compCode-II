45bca655b4c175f2320e2afacdf7282a3b2c37d2
vismit2000 <44922474+vismit2000@users.noreply.github.com>
2019-08-04 16:44:55 +0530

    LPS : Bottom up DP
    https://leetcode.com/problems/longest-palindromic-subsequence/

---
+++ Longest_Palindromic_Subsequence.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/longest-palindromic-subsequence/
+
+class Solution {
+public:
+    int dp[1005][1005];
+    
+    int longestPalindromeSubseq(string s) 
+    {
+        int n = s.length();
+        memset(dp, 0, sizeof(dp));
+        
+	    for(int i = 0; i < n; i++) 
+		    dp[i][i] = 1;
+        
+        for(int len = 2; len <= n; len++) 
+        {
+            for(int i = 0; i <= n-len; i++) 
+            {
+                int j = i + len - 1;
+                
+                if(s[i] == s[j]) 
+                    dp[i][j] = 2 + dp[i+1][j-1];
+                else 
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1];
+    }
+};
