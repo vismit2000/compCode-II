28a78708299a55c85f51278a675d981fed3f3771
vismit2000 <44922474+vismit2000@users.noreply.github.com>
2019-08-25 21:17:40 +0530

    Kickstart Round-E 2019: Fully Solved Problem using Factoriazation and Prime Sieve

--- Street_Checkers.cpp
+++ Street_Checkers.cpp
@@ -49,3 +49,121 @@ Thus, the answer for this test case is 5.
 
 In the second sample case, we have only one number 102. Alice would paint 4 tiles : {1, 3, 17, 51} while Bob would paint 4 tiles : {2, 6, 34, 102}. The game would be interesting since the absolute difference is 0.
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int p[1000001];
+
+vector<int> primeSieve()
+{
+    memset(p, 0, sizeof(p));
+    int n = 1000001;
+    p[0] = p[1] = 0;
+    p[2] = 1;
+
+    for(int i = 3; i <= n; i += 2)
+        p[i] = 1;
+
+    for(ll i = 3; i <= n; i += 2)
+    {    
+        if(p[i])
+        {
+            for(ll j = i*i; j <= n; j += 2*i)
+            {
+                p[j] = 0;
+            }
+        }
+    }
+
+    vector<int> primes;
+    primes.push_back(2);
+    
+    for(int i = 3; i < n; i += 2)
+    {
+        if(p[i] == 1)
+        {
+            primes.push_back(i);
+        }
+    }
+    return primes;
+}
+
+//This function returns a vector of prime factors with their count
+vector< pair < int, int > > factorize(int m,vector <int> &primes)
+{
+    vector< pair < int, int > > factors;
+
+    int i = 0, cnt;
+    int p = primes[0];
+
+    while(p*p <= m)
+    {
+        if(m%p == 0)
+        {
+            cnt = 0;
+            while(m % p == 0)
+            {
+                cnt++;
+                m = m/p;
+            }
+            factors.push_back({p, cnt});
+        }
+        i++;
+        p = primes[i];
+    }
+
+    //One Last Check for a prime
+    if(m!=1){
+        factors.push_back({m, 1});
+    }
+    return factors;
+}
+
+int main() 
+{
+    int t;
+    cin>>t;
+
+    vector<int> primes = primeSieve();
+
+    for(int q = 1; q <= t; q++)
+    {
+        cout<<"Case #"<<q<<": ";
+        int L, R;
+        cin >> L >> R;
+
+        int ans = 0;
+
+        for(int i = L; i <= R; i++)
+        {
+            vector< pair < int, int > > factors = factorize(i, primes);
+            int cnt_odd = 0, cnt_even = 0;
+
+            if(i % 2 != 0)
+            {
+                cnt_even = 0;
+                cnt_odd = 1;
+                for(auto group : factors)
+                    cnt_odd *= (1 + group.second);
+            }
+            else
+            {
+                cnt_even = 1;
+                for(auto group : factors)
+                    cnt_even *= (1 + group.second);
+
+                cnt_odd = cnt_even/(1 + factors[0].second);
+
+                cnt_even -= cnt_odd;
+            }
+
+            if(abs(cnt_even - cnt_odd) <= 2)
+                ans++;            
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
