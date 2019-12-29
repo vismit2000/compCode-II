2f4e3224181c1474235da2f625d871b0c318f107
vismit2000 <44922474+vismit2000@users.noreply.github.com>
2019-12-29 20:04:56 +0530

    Update My_Template.cpp

--- My_Template.cpp
+++ My_Template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll lonf long
+#define ll long long
 #define INF 0x3f3f3f3f
 #define MOD 1000000007
 #define boost ios_base ::sync_with_stdio(0); cin.tie(0);
