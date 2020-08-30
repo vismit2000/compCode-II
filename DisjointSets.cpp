a9ffbdbaa9799f5fe8b9a3e622a822e3ee1b936d
Vishal Mittal <f20170080@pilani.bits-pilani.ac.in>
2020-08-30 13:49:02 +0530

    Template for Disjoint Sets (Union-Find)

---
+++ DisjointSets.cpp
@@ -0,0 +1,36 @@
+class DisjointSets{ 
+    int *parent, *rnk; 
+    int n; 
+ public: 
+    DisjointSets(int n) { 
+        this->n = n; 
+        parent = new int[n+1]; 
+        rnk = new int[n+1]; 
+ 
+        for (int i = 0; i <= n; i++) { 
+            rnk[i] = 0; 
+            parent[i] = i; 
+        } 
+    } 
+  
+    // Find the parent of a node 'u' - Path Compression 
+    int findParent(int u){ 
+        if (u != parent[u]) 
+            parent[u] = findParent(parent[u]); 
+        return parent[u]; 
+    } 
+    
+    // Union by rank
+    void merge(int x, int y){ 
+        x = findParent(x), y = findParent(y); 
+  
+        // Make tree with smaller height a subtree of the other tree
+        if (rnk[x] > rnk[y]) 
+            parent[y] = x; 
+        else // If rnk[x] <= rnk[y] 
+            parent[x] = y; 
+  
+        if (rnk[x] == rnk[y]) 
+            rnk[y]++; 
+    } 
+}; 
