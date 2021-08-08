2de7338077050f5c431a7dcb5a41905aa339ad07
Vishal Mittal <f20170080@pilani.bits-pilani.ac.in>
2021-08-08 19:49:20 +0530

    Update DisjointSets.cpp

--- DisjointSets.cpp
+++ DisjointSets.cpp
@@ -34,3 +34,29 @@ class DisjointSets{
             rnk[y]++; 
     } 
 }; 
+
+
+// Better:
+
+class DisjointSets {
+    unordered_map < int, int > parent, rnk;
+public:
+    int findParent(int u){ 
+        if (!parent.count(u)) parent[u] = u;
+        if (u != parent[u]) parent[u] = findParent(parent[u]); 
+        return parent[u]; 
+    } 
+    
+    void merge(int x, int y){ 
+        x = findParent(x), y = findParent(y); 
+  
+        // Make tree with smaller height a subtree of the other tree
+        if (rnk[x] > rnk[y]) 
+            parent[y] = x; 
+        else // If rnk[x] <= rnk[y] 
+            parent[x] = y; 
+  
+        if (rnk[x] == rnk[y]) 
+            rnk[y]++; 
+    }
+};
