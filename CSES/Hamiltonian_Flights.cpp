beb990923c9cddd3ad1b5909b227b0f6376f1578
Vishal Mittal <f20170080@pilani.bits-pilani.ac.in>
2024-04-16 19:00:44 +0530

    DP Bitmask TSP
    DP Bitmask TSP

---
+++ CSES/Hamiltonian_Flights.cpp
@@ -0,0 +1,39 @@
+// CSES Hamiltonian Flights: https://cses.fi/problemset/task/1690
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define boost ios_base ::sync_with_stdio(0); cin.tie(0);
+
+map < pair < ll, ll >, ll > dp;
+
+ll tsp(ll mask, ll pos, ll n, vector < vector < ll > > &v){
+    if(mask == (1 << n) - 1 && pos == n - 1) return 1;
+    if(dp.count({mask, pos})) return dp[{mask, pos}];
+    ll ans = 0;
+    for(int i = 0; i < n; i++){
+        if((mask & (1 << i)) == 0)
+            ans = (ans + tsp(mask | (1 << i), i, n, v)) % MOD;
+    }
+    return dp[{mask, pos}] = ans;
+}
+
+
+int main() {
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ll n, m; cin >> n >> m;
+    vector < vector < ll > > v(n);
+    ll a, b;
+    for(int i = 0; i < m; i++){
+        cin >> a >> b;
+        v[a - 1].push_back(b - 1);
+    }
+    cout << tsp(1, 0, n, v) << endl;
+}
